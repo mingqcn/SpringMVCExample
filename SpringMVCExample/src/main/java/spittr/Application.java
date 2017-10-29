4e47b68cd029aacfddf9d231027d6db20bde7505
mingqcn <mingqiu@xmu.edu.cn>
2017-10-29 21:44:46 +0800

    第三次修改

--- SpringMVCExample/src/main/java/spittr/Application.java
+++ SpringMVCExample/src/main/java/spittr/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * 
+ * Application类
  * @author mingqiu
  *
  */
